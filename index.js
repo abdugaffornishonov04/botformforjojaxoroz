c19d12587abe8c2465de59a72c391ef27c1f0f80
Devgaffor <abdugaffor0042@gmail.com>
2024-01-09 23:10:56 +0500

    done

--- index.js
+++ index.js
@@ -1,52 +1,23 @@
-let daysButton = document.querySelector( ".days-btn" );
 let daysDropDown = document.querySelector( ".days-dropdown" );
 let daysGetButtons = document.querySelectorAll( ".days-getter-buttons" );
+
+let doneDiv = document.querySelector( ".done-div");
 
 let submitButton = document.querySelector( ".submit-btn" );
 let form = document.querySelector( ".form" );
 
-daysButton.addEventListener( "click", () => {
-  daysDropDown.classList.toggle( "days-dropdown-2" )
-} )
+form.addEventListener( "submit", function ( e ) {
+  e.preventDefault();
 
-daysGetButtons.forEach( button => {
-  button.addEventListener( "click", function ( el ) {
-    console.log( el.target.value );
+  let nameValue = document.getElementById( "name" ).value;
+  let daysValue = document.getElementById( "selectform" ).value;
+  let timeValue = document.getElementById( "time" ).value;
+  let reminderValue = document.getElementById( "reminder" ).value;
 
-    daysButton.textContent = el.target.value
-    daysDropDown.classList.remove( "days-dropdown-2" )
+  console.log( "Name:", nameValue );
+  console.log( "Days:", daysValue );
+  console.log( "Time:", timeValue );
+  console.log( "Reminder:", reminderValue );
 
-  } );
-} );
-
-// form.addEventListener( 'submit', ( event ) => {
-//   event.preventDefault();
-
-//   const inputValue = document.getElementById( 'name' ).value;
-//   const inputValue2 = document.getElementById( 'days' ).value;
-//   const inputValue3 = document.getElementById( 'time' ).value;
-//   const inputValue4 = document.getElementById( 'reminder' ).value;
-
-//   console.log( 'Input value:', inputValue );
-//   console.log( 'Input value:', inputValue2 );
-//   console.log( 'Input value:', inputValue3 );
-//   console.log( 'Input value:', inputValue4 );
-// } );
-
-document.addEventListener( 'DOMContentLoaded', () => {
-  const form = document.querySelector( '.form' );
-
-  form.addEventListener( 'submit', ( event ) => {
-    event.preventDefault();
-
-    const inputValue = document.getElementById( 'name' ).value;
-    const inputValue2 = document.getElementById( 'day' ).value;
-    const inputValue3 = document.getElementById( 'time' ).value;
-    const inputValue4 = document.getElementById( 'reminder' ).value;
-
-    console.log( 'Input value:', inputValue );
-    console.log( 'Input value:', inputValue2 );
-    console.log( 'Input value:', inputValue3 );
-    console.log( 'Input value:', inputValue4 );
-  } );
+  doneDiv.classList.add("done-div2")
 } );
